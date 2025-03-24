df9a168fbfc2a33cf4cb8f43777329d63894ddc1
Mijura <miodragvilotijevic@gmail.com>
2025-03-24 17:12:31 +0100

    package.json update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// Import the generated code file
 import { dynamicGreeting } from './../dist/generated'; // The generated file from build time
 
 console.log("Executing dynamically generated function:");
